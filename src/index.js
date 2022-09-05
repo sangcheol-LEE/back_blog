4bfd304e7aec87e875950d364b86075f2b3bf2da
이상철 <zlrz6706@gmail.com>
2022-09-05 23:17:29 +0900

    몽고디비 사용준비 끝

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require("koa-bodyparser");
+const mongoose = require("mongoose");
+
 
 const api = require("./api")
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+.connect(MONGO_URI, { useNewUrlParser :true})
+  .then(() => {
+    console.log("Connected to Mongo DB");
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
 
 const app = new Koa();
 const router = new Router();
@@ -13,7 +28,7 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-
-app.listen(4000, ()=>{
-  console.log("listening to port 4000")
-})
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log("listening to port %d", port);
+});
