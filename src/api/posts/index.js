dcc4f1672561ca0a1944e516c187aae9306f49d9
이상철 <zlrz6706@gmail.com>
2022-09-07 22:15:03 +0900

    register

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from "koa-router";
 import * as postsCtrl from "./posts.ctrl";
+import checkLoggedIn from "../../lib/checkLoggedIn";
 
 const posts = new Router();
 
 posts.get("/", postsCtrl.list);
-posts.post("/", postsCtrl.write);
+posts.post("/", checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
-posts.get("/:id", postsCtrl.checkObjectId ,postsCtrl.read);
-posts.delete("/:id", postsCtrl.checkObjectId ,postsCtrl.remove);
-posts.patch("/:id", postsCtrl.checkObjectId ,postsCtrl.update);
+posts.get("/" ,postsCtrl.read);
+posts.delete("/", checkLoggedIn ,postsCtrl.remove);
+posts.patch("/", checkLoggedIn ,postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes())
 
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import  mongoose from "mongoose";
 
 
 import api from "./api"
-import createFakeData from './createFakeData';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -27,6 +27,7 @@ const router = new Router();
 router.use("/api", api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
 
